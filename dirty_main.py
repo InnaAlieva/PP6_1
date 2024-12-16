3ad4518e1ef67e7eaebfd9410734cb6429c966bd
Inna Alieva <alievainna123@gmail.com>
2024-12-16 19:52:48 +0300

    Initial commit

---
+++ dirty_main.py
@@ -0,0 +1,7 @@
+from main import *
+
+
+if __name__ == '__main__':
+
+    get_employees()
+    calculate_salary()
---
+++ main.py
@@ -0,0 +1,11 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+from datetime import datetime
+
+
+print(datetime.now().replace(microsecond=0))
+
+if __name__ == '__main__':
+
+    get_employees()
+    calculate_salary()
